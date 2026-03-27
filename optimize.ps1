47b15021bf102984e5b53ac0e0821367bcecc8f4
Aman J Sonal <79847765+Aman-290@users.noreply.github.com>
2026-03-27 09:30:33 +0530

    meta data

--- optimize.ps1
+++
@@ -1,8 +0,0 @@
-$images = Get-ChildItem -Recurse -Include *.png, *.jpg, *.jpeg -Exclude *.webp
-foreach ($img in $images) {
-    if ($img.Extension -ne ".webp") {
-        $out = [System.IO.Path]::ChangeExtension($img.FullName, ".webp")
-        ffmpeg -y -i $img.FullName -vf "scale='if(gt(iw,1024),1024,-1)':'if(gt(ih,1024),1024,-1)',scale=iw:ih:force_original_aspect_ratio=decrease" -q:v 75 $out
-        Remove-Item $img.FullName
-    }
-}
